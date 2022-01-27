1eb97e065dc67c52396b4da94cc82ed47294e725
zjw <168762321@qq.com>
2022-01-27 19:34:48 +0800

    构建UI

---
+++ dependencies/glfw/src/CMakeFiles/glfw_objects.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/context.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/context.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/egl_context.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/egl_context.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/glx_context.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/glx_context.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/init.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/init.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/input.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/input.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/linux_joystick.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/linux_joystick.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/monitor.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/monitor.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/osmesa_context.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/osmesa_context.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/posix_thread.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/posix_thread.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/posix_time.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/posix_time.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/vulkan.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/vulkan.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/window.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/window.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/x11_init.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/x11_init.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/x11_monitor.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/x11_monitor.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/x11_window.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/x11_window.c.o"
+  "/home/zjw/work_files/holemap/dependencies/glfw/src/xkb_unicode.c" "/home/zjw/work_files/holemap/dependencies/glfw/src/CMakeFiles/glfw_objects.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_BUILD_DLL"
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "dependencies"
+  "dependencies/glfw/include"
+  "dependencies/glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ include/base.h
@@ -0,0 +1,18 @@
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <imgui/ImGuizmo.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+//filebrowser
+#include <filebrowser/imfilebrowser.h>
+
+
+
+
+using namespace ImGui;
